adfc2918028716d24a0912590222fb20164c250e
Siddharth Surana <sddhrthsrn119@gmail.com>
2026-06-15 00:11:36 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> solution;
+        for(int i=0;i<nums.size()-1;i++){
+            for(int j=i+1;j<=nums.size()-1;j++){
+                if(nums[i]+nums[j]==target){
+                    solution.push_back(i);
+                    solution.push_back(j);
+                    break;
+                }
+            }
+        }
+        return solution;
+        
+    }
+};
