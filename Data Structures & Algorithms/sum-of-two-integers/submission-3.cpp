ac4f738b89bcec5699ace4178be428e5630e4e92
Siddharth Surana <sddhrthsrn119@gmail.com>
2026-08-17 01:09:12 +0530

    Bulk sync: 21 submissions

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-3.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int getSum(int a, int b) {
+        int sum = a^b;
+        int carry = (a&b)<<1;
+
+        while(carry!=0){
+            int NewCarry = (sum & carry)<<1;
+            sum = sum^carry;
+            carry = NewCarry;
+        }
+        return sum;
+    }
+};
